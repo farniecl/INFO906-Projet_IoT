3daffe841eef9748a67c9cc5252d3a550cf0b427
ClementFarnier <clement.farnier@free.fr>
2017-03-07 10:36:05 +0100

    Premier commit de l'application Android. Requêtes Rest commencées, non terminées

---
+++ android/Temperapp/app/src/main/java/fr/usmb/farnier/temperapp/MainActivity.java
@@ -0,0 +1,67 @@
+package fr.usmb.farnier.temperapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.btn_get);
+        final TextView tv = (TextView) findViewById(R.id.tv_get);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tv.setText(/*getRequest()*/"Coucou");
+            }
+        });
+    }
+
+    private String getRequest() {
+        String output = "";
+        try {
+
+            URL url = new URL("http://localhost:8080/RESTfulExample/json/product/get");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+        return output;
+    }
+}
