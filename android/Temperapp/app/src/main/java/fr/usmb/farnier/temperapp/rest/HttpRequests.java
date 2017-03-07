9198ebf007d7ea9cb1467a4e7955a180d8c1f3fa
ClementFarnier <clement.farnier@free.fr>
2017-03-07 15:12:40 +0100

    MOD_CF_20170307 API Rest client terminée / Non testée

---
+++ android/Temperapp/app/src/main/java/fr/usmb/farnier/temperapp/rest/HttpRequests.java
@@ -0,0 +1,132 @@
+package fr.usmb.farnier.temperapp.rest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by clement on 03/03/17.
+ */
+
+public class HttpRequests implements IHttpRequest {
+
+    final static private String HTTP_CODE_GET = "GET";
+    final static private String HTTP_CODE_POST = "POST";
+
+    /**
+     *
+     * @param aUrl Request url
+     * @param aRequestProperty Request property (Access / Content-Type)
+     * @param aRequestType Request type (GET / POST)
+     * @return {@link HttpURLConnection}
+     */
+    private HttpURLConnection getHttpURLConnection(String aUrl, String aRequestProperty, String aRequestType) {
+        try {
+            URL url = new URL(aUrl);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod(aRequestType);
+            con.setRequestProperty(aRequestProperty, "application/json");
+            return con;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     *
+     * @param aCon {@link HttpURLConnection}
+     */
+    private void checkHttpURLConnectionException(HttpURLConnection aCon) {
+        try {
+            if (aCon.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + aCon.getResponseCode());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * @param aCon {@link HttpURLConnection}
+     * @return {@link BufferedReader}
+     */
+    private BufferedReader getBufferedReader(HttpURLConnection aCon) {
+        try {
+            return new BufferedReader(new InputStreamReader((aCon.getInputStream())));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     *
+     * @param aBr {@link BufferedReader}
+     * @return {@link JSONObject}
+     */
+    private JSONObject bufferedReaderToJsonObject(BufferedReader aBr) {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try {
+            while ((line = aBr.readLine()) != null) {
+                sb.append(line);
+            }
+            JSONObject json = new JSONObject(sb.toString());
+            return json;
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     *
+     * @param aJson {@link JSONObject}
+     */
+    private void checkJsonObject(JSONObject aJson) {
+        try {
+            if (aJson == null) {
+                throw new Exception("La chaine de caractère JSON est null");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public JSONObject HttpGetRequest(String aUrl) {
+        HttpURLConnection con = getHttpURLConnection(aUrl, "Accept", HTTP_CODE_GET);
+        checkHttpURLConnectionException(con);
+        JSONObject json = bufferedReaderToJsonObject(getBufferedReader(con));
+        checkJsonObject(json);
+        con.disconnect();
+        return json;
+    }
+
+    @Override
+    public JSONObject HttpPostRequest(String aUrl, String aInput) {
+        HttpURLConnection con = getHttpURLConnection(aUrl, "Content-Type", HTTP_CODE_POST);
+        checkHttpURLConnectionException(con);
+        try {
+            OutputStream os = con.getOutputStream();
+            os.write(aInput.getBytes());
+            os.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        checkHttpURLConnectionException(con);
+        JSONObject json = bufferedReaderToJsonObject(getBufferedReader(con));
+        checkJsonObject(json);
+        con.disconnect();
+        return json;
+    }
+}
---
+++ android/Temperapp/app/src/main/java/fr/usmb/farnier/temperapp/rest/IHttpRequest.java
@@ -0,0 +1,24 @@
+package fr.usmb.farnier.temperapp.rest;
+
+import org.json.JSONObject;
+
+/**
+ * Created by clement on 07/03/17.
+ */
+
+public interface IHttpRequest {
+    /**
+     *
+     * @param aUrl Request url
+     * @return {@link JSONObject}
+     */
+    JSONObject HttpGetRequest(String aUrl);
+
+    /**
+     *
+     * @param aUrl Request url
+     * @param aInput Request (json format)
+     * @return {@link JSONObject}
+     */
+    JSONObject HttpPostRequest(String aUrl, String aInput);
+}
