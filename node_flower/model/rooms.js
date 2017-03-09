ae79815cce9110a9a2bd194a60e76db953b818b2
Thibaut <thibaut.martin1@etu.univ-savoie.fr>
2017-03-09 15:30:18 +0100

    ajout temp

--- node_flower/model/rooms.js
+++ node_flower/model/rooms.js
@@ -1,46 +1,52 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rooms');
-var Room = mongoose.model('Room');
-var Schema = mongoose.Schema
+var sync = require('synchronize');
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/rooms',function(err) {
+  if(err) console.log(err);
+});
+var Rooms = mongoose.model('Rooms', new mongoose.Schema({
+    'id': String,
+    'out_temp':Number,
+    'in_temp':Number,
+    'state_fences':Boolean,
+    'state_windows':Boolean
+}));
 
 exports.setTemp = function (id,temp) {
-  Room.findById(id, function (err, room) {
+  Rooms.findOne({ 'id': id }).exec(function (err, room) {
     if (!err) {
-      room.temp = temp;
-      post.save(function (err) {
-      // do something
-      });
-    }
+        if(room){
+          room.in_temp = temp;
+          room.save(function (err) {
+            if(err) console.log(err);
+          });
+        }
+      }
   });
 };
 
 exports.setFenceState = function (id,state) {
-  Room.findById(id, function (err, room) {
+  Rooms.findOne({ 'id': id }, function (err, room) {
     if (!err) {
       room.state_fences = state;
-      post.save(function (err) {
-      // do something
+      room.save(function (err) {
+        if(err) console.log(err);
       });
     }
   });
 };
 
 exports.setWindowState = function (id,state) {
-  Room.findById(id, function (err, room) {
+  Rooms.findOne({ 'id': id }, function (err, room) {
     if (!err) {
       room.state_windows = state;
-      post.save(function (err) {
-      // do something
+      room.save(function (err) {
+        if(err) console.log(err);
       });
     }
   });
 };
 
-exports.getRoomById = function(id){
-  Room.findById(id, function (err, room) {
-    if (!err) {
-      return room;
-      });
-    }
-  });
+exports.getRoomById = function(id,callback){
+  Rooms.findOne({ 'id': id },callback);
 };
