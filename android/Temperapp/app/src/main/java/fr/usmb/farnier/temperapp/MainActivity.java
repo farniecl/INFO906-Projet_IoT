1cdabcf83438cf924e8ecfc82d775f9a321b7e25
ClementFarnier <clement.farnier@free.fr>
2017-03-09 09:54:54 +0100

    Finalisation de l'interface android

--- android/Temperapp/app/src/main/java/fr/usmb/farnier/temperapp/MainActivity.java
+++ android/Temperapp/app/src/main/java/fr/usmb/farnier/temperapp/MainActivity.java
@@ -2,20 +2,17 @@ package fr.usmb.farnier.temperapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    final String ROOM_NUMBER = "Chambre 000";
-    ListView mListView;
-    String[] rooms = new String[]{
+    private String room_number = "000";
+    private String[] rooms = new String[]{
             "Chambre 001", "Chambre 002"
     };
 
@@ -25,24 +22,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mListView = (ListView) findViewById(R.id.lv_room);
+        ListView mListView = (ListView) findViewById(R.id.lv_room);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
                 android.R.layout.simple_list_item_1, rooms);
         mListView.setAdapter(adapter);
 
+        // Listener de la liste
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, android.view.View view, int position, long id) {
-
-
-                // toast (notif de bas de page
                 String room_name = (String) ((TextView)view).getText();
-
-
-                //passage à l'activite suivante
+                if (room_name.equals("Chambre 001"))
+                    room_number = "001";
+                else if (room_name.equals("Chambre 002"))
+                    room_number = "002";
                 Intent intent = new Intent(MainActivity.this, RoomActivity.class);
-                intent.putExtra(ROOM_NUMBER,room_name);
+                intent.putExtra("room_number",room_number);
                 startActivity(intent);
             }
 
--- android/Temperapp/app/src/main/java/fr/usmb/farnier/temperapp/RoomActivity.java
+++ android/Temperapp/app/src/main/java/fr/usmb/farnier/temperapp/RoomActivity.java
@@ -10,6 +10,7 @@ import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,7 +24,7 @@ import fr.usmb.farnier.temperapp.rest.IHttpRequests;
 public class RoomActivity extends Activity {
 
 
-    private String room_number = "Chambre 000";
+    private String room_number = "000";
     private Double int_temp = 00.0;
     private Boolean state_fences = false;
     private Boolean state_windows = false;
@@ -46,9 +47,12 @@ public class RoomActivity extends Activity {
         final Switch sw_fences = (Switch) findViewById(R.id.sw_fences);
         final Switch sw_windows = (Switch) findViewById(R.id.sw_windows);
 
+        // Récupération des données de l'activité précédente
+        room_number = intent.getStringExtra("room_number");
+
         // Récupération du numéro de chambre
         if (intent != null) {
-            tv_room.setText(intent.getStringExtra(room_number));
+            tv_room.setText("Chambre " + room_number);
         }
 
         // Récupération de l'heure courante
@@ -57,14 +61,14 @@ public class RoomActivity extends Activity {
 
         // Récupération des données du serveur
         IHttpRequests hr = new HttpRequests();
-        JSONObject json = hr.HttpGetRequest("http://192.168.43.197:3000/room/001");
+        JSONObject json = hr.HttpGetRequest("http://192.168.43.197:3000/room/" + room_number);
         try {
             tv_tempext.setText(json.getString("out_temp") + "°C");
             tv_tempint.setText(json.getString("in_temp") + "°C");
             Double doubleTemp = json.getDouble("in_temp") * 10;
             int_temp = json.getDouble("in_temp");
             sb_temp.setProgress(doubleTemp.intValue());
-            if (!json.getBoolean("state_fences")) {
+            if (json.getBoolean("state_fences")) {
                 sw_fences.toggle();
                 sw_fences.setChecked(true);
             }
@@ -86,9 +90,8 @@ public class RoomActivity extends Activity {
         sb_temp.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
             @Override
             public void onProgressChanged(SeekBar sb_temp, int progress, boolean fromUser) {
-                // TODO Auto-generated method stub
-                tv_tempint.setText(String.valueOf(String.valueOf(progress/10)) + "." +
-                        String.valueOf(progress%10) + "°C");
+                int_temp = Double.valueOf(String.valueOf(progress/10) + "." + String.valueOf(progress%10));
+                tv_tempint.setText(int_temp + "°C");
             }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {}
@@ -96,25 +99,19 @@ public class RoomActivity extends Activity {
             public void onStopTrackingTouch(SeekBar seekBar) {}
         });
 
+        // Listener du switch pour les volets
         sw_fences.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    state_fences = true;
-                } else {
-                    state_fences = false;
-                }
+                state_fences = isChecked;
             }
         });
 
+        // Listener du switch pour la fenêtre
         sw_windows.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    state_windows = true;
-                } else {
-                    state_windows = false;
-                }
+                state_windows = isChecked;
             }
         });
 
@@ -123,7 +120,21 @@ public class RoomActivity extends Activity {
         bt_validate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO
+                JSONObject json = new JSONObject();
+                try {
+                    json.put("in_temp", int_temp);
+                    json.put("state_fences", state_fences);
+                    json.put("state_windows", state_windows);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                IHttpRequests hr = new HttpRequests();
+                JSONObject result = hr.HttpPostRequest("http://192.168.43.197:3000/room/" + room_number, json.toString());
+                try {
+                    Toast.makeText(getBaseContext(), result.getString("result"), Toast.LENGTH_LONG).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
